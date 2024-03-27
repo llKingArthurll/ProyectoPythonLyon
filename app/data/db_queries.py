357f22411b8e8c5545e350a96d583d9303f1e9fa
Arthur <guillermoarturof8@gmail.com>
2024-03-27 16:25:14 -0500

    Se agregó la pestaña de búsqueda.

--- app/data/db_queries.py
+++ app/data/db_queries.py
@@ -5,29 +5,98 @@ class DatabaseQueries:
 
     def _execute_query(self, query, parameters=None):
         cursor = self.db_connection.connection.cursor()
-        cursor.execute(query, parameters)
+        if parameters:
+            cursor.execute(query, parameters)
+        else:
+            cursor.execute(query)
         self.db_connection.commit()
-        return cursor.fetchall()  # Retorna los resultados para su recuperación
+        return cursor.fetchall()
 
     def insert_nuevo_registro(self, data):
         query = """
-            INSERT INTO NuevoIngreso
-            (nombre_guia, nombre_empresa, cantidad_productos, fecha_guia, save_data, path_guia, path_factura,
-             nombre_producto, descripcion_producto, series_producto)
-            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+        INSERT INTO NuevoIngreso
+        (nombre_guia, nombre_empresa, cantidad_productos, fecha_guia, save_data, path_guia, path_factura,
+        nombre_producto, descripcion_producto, series_producto)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
         """
         self._execute_query(query, data)
 
-    # def select_last_nuevo_registro_id(self):
-    #     query = "SELECT id_NuevoIngreso FROM NuevoIngreso ORDER BY id_NuevoIngreso DESC LIMIT 1"
-    #     result = self._execute_query(query)
-    #     return result[0][0] if result else None  # Accede al primer elemento de la primera fila
-
-    def list_resumen_nuevoingreso(self):
+    def list_resumen_nuevoingreso(self, id_nuevoingreso=None):
         query = """
-            SELECT id_NuevoIngreso AS id, nombre_guia AS guia, nombre_empresa AS empresa,
-                   cantidad_productos AS cantidadproductos, fecha_guia AS fecha
-            FROM NuevoIngreso
+        SELECT id_NuevoIngreso AS id, nombre_guia AS guia, nombre_empresa AS empresa,
+        cantidad_productos AS cantidadproductos, fecha_guia AS fecha
+        FROM NuevoIngreso
         """
         return self._execute_query(query)
 
+    def search_products_by_id_nombre(self, id_nuevoingreso):
+        query = """
+        SELECT nombre_producto
+        FROM NuevoIngreso
+        WHERE id_NuevoIngreso = ?
+        """
+        parameters = [id_nuevoingreso]
+        return self._execute_query(query, parameters)[0][0]
+
+    def search_products_by_id_descripcion(self, id_nuevoingreso):
+        query = """
+        SELECT descripcion_producto
+        FROM NuevoIngreso
+        WHERE id_NuevoIngreso = ?
+        """
+        parameters = [id_nuevoingreso]
+        return self._execute_query(query, parameters)[0][0]
+
+    def search_products_by_id_serie(self, id_nuevoingreso):
+        query = """
+        SELECT series_producto
+        FROM NuevoIngreso
+        WHERE id_NuevoIngreso = ?
+        """
+        parameters = [id_nuevoingreso]
+        return self._execute_query(query, parameters)[0][0]
+
+    def search_by_guia(self, texto_busqueda):
+        query = """
+        SELECT id_NuevoIngreso
+        FROM NuevoIngreso
+        WHERE nombre_guia LIKE ?
+        """
+        parameters = [texto_busqueda + '%']
+        return self._execute_query(query, parameters)
+
+    def search_by_empresa(self, texto_busqueda):
+        query = """
+        SELECT id_NuevoIngreso
+        FROM NuevoIngreso
+        WHERE nombre_empresa LIKE ?
+        """
+        parameters = [texto_busqueda + '%']
+        return self._execute_query(query, parameters)
+
+    def search_by_nombre_producto(self, texto_busqueda):
+        query = """
+        SELECT id_NuevoIngreso
+        FROM NuevoIngreso
+        WHERE nombre_producto LIKE ?
+        """
+        parameters = ['%' + texto_busqueda + '%']
+        return self._execute_query(query, parameters)
+    
+    def search_by_serie(self, serie):
+        query = """
+        SELECT id_NuevoIngreso
+        FROM NuevoIngreso
+        WHERE series_producto LIKE ?
+        """
+        parameters = ['%' + serie + '%']
+        return self._execute_query(query, parameters)
+
+    def obtener_datos_completos(self, id_nuevoingreso):
+        query = """
+        SELECT nombre_guia AS guia, nombre_empresa AS empresa, cantidad_productos AS cantidadproductos, fecha_guia AS fecha
+        FROM NuevoIngreso
+        WHERE id_NuevoIngreso = ?
+        """
+        parameters = [id_nuevoingreso]
+        return self._execute_query(query, parameters)
--- app/views/buscar_producto_view.py
+++ app/views/buscar_producto_view.py
@@ -1,86 +1,184 @@
 import tkinter as tk
 from tkinter import ttk
+from tkinter import messagebox
+from app.data.db_connection import DatabaseConnection
+from app.data.db_queries import DatabaseQueries
+from app.views.buscar_producto_especifico_view import BuscarProductoEspecificoView
 
 class BuscarProductoView:
-
     def __init__(self, root, controller):
-        # Inicializa la vista para buscar un producto.
-
-        # Args:
-        #     root (tk.Tk): La ventana principal de la aplicación.
-        #     controller (object): El controlador de la aplicación.
         self.root = root
         self.controller = controller
         self.root.title("Buscar Producto")
+        self.root.geometry("800x600")
+
+        # Conexión a la base de datos
+        self.db_queries = DatabaseQueries(DatabaseConnection())
 
         # Agregar título
         self.titulo_label = tk.Label(self.root, text="Buscar Producto", font=("Arial", 14))
         self.titulo_label.pack(pady=10)
 
-        # Agregar barra de búsqueda
-        self.search_entry = tk.Entry(self.root, width=50)
-        self.search_entry.pack(pady=10)
+        # Frame para el cuadro de búsqueda y los botones
+        search_frame = tk.Frame(self.root)
+        search_frame.pack(pady=10)
+
+        # Agregar cuadro de búsqueda con borde redondeado y color de borde blanco
+        self.search_entry = tk.Entry(search_frame, width=30, font=("Arial", 11))
+        self.search_entry.grid(row=0, column=0, padx=20)
+
+        # Agregar botón de búsqueda
+        self.search_button = tk.Button(search_frame, text="Buscar", command=self.buscar_en_tabla)
+        self.search_button.grid(row=0, column=1, padx=10)
+
+        # Agregar botón de borrar
+        self.clear_button = tk.Button(search_frame, text="Borrar", command=self.borrar_tabla)
+        self.clear_button.grid(row=0, column=2, padx=10)
 
         # Agregar tabla
-        self.tabla_frame = tk.Frame(self.root)
-        self.tabla_frame.pack(fill="both", expand=True)
+        tabla_frame = tk.Frame(self.root, padx=20, pady=20)
+        tabla_frame.pack(expand=True)
 
-        self.scroll_frame = tk.Frame(self.tabla_frame)
-        self.scroll_frame.pack(fill="both", expand=True)
+        self.tabla = ttk.Treeview(tabla_frame, columns=("ID", "Guía", "Empresa", "Cantidad", "Fecha"), show="headings")
+        self.tabla.pack(expand=True, fill="both")
 
-        self.canvas = tk.Canvas(self.scroll_frame)
-        self.canvas.pack(side="left", fill="both", expand=True)
+        # **Ajustar el ancho de las columnas**
+        self.tabla.column("ID", width=50, anchor="center")
+        self.tabla.column("Guía", width=120, anchor="center")
+        self.tabla.column("Empresa", width=250, anchor="center")
+        self.tabla.column("Cantidad", width=100, anchor="center")
+        self.tabla.column("Fecha", anchor="center")
 
-        self.scrollbar = ttk.Scrollbar(self.scroll_frame, orient="vertical", command=self.canvas.yview)
-        self.scrollbar.pack(side="right", fill="y")
-        self.canvas.configure(yscrollcommand=self.scrollbar.set)
+        # Definir encabezados
+        self.tabla.heading("ID", text="ID")
+        self.tabla.heading("Guía", text="Guía")
+        self.tabla.heading("Empresa", text="Empresa")
+        self.tabla.heading("Cantidad", text="Cantidad")
+        self.tabla.heading("Fecha", text="Fecha")
 
-        self.inner_frame = tk.Frame(self.canvas)
-        self.inner_frame_id = self.canvas.create_window((0, 0), window=self.inner_frame, anchor="nw")
-
-        # Agregar encabezados de la tabla
-        self.mostrar_label_tabla("Nombre", 0, 0)
-        self.mostrar_label_tabla("Descripción", 1, 0)
-        self.mostrar_label_tabla("Series", 2, 0)
-
-        # Agregar filas de la tabla (ejemplo)
-        for i in range(1, 4):
-            self.mostrar_label_tabla(f"Producto {i}", 0, i)
-            self.mostrar_label_tabla(f"Descripción del producto {i}", 1, i)
-            self.mostrar_label_tabla(f"Serie 1, Serie 2", 2, i)
+        # Mostrar resumen de 'NuevoIngreso'
+        self.mostrar_resumen_nuevoingreso()
 
         # Agregar botón para volver a opciones
         self.volver_button = tk.Button(self.root, text="Volver a Opciones", command=self.volver_a_opciones)
-        self.volver_button.pack(pady=10)
+        self.volver_button.pack(side="bottom", pady=10)
 
-    def mostrar_label_tabla(self, text, columna, fila):
-        """
-        Muestra una etiqueta en la tabla.
+        # Variable para almacenar ID seleccionado
+        self.selected_id = None
+        
+        # Vincular el evento de presionar la tecla "Enter" con la función buscar_en_tabla
+        self.search_entry.bind("<Return>", lambda event: self.buscar_en_tabla())
 
-        Args:
-            text (str): El texto de la etiqueta.
-            columna (int): La columna de la etiqueta.
-            fila (int): La fila de la etiqueta.
-        """
-        label = tk.Label(self.inner_frame, text=text)
-        label.grid(column=columna, row=fila, pady=5)
+    def mostrar_resumen_nuevoingreso(self):
+        resumen_nuevoingreso = self.db_queries.list_resumen_nuevoingreso()
+
+        # Insertar datos
+        for i, registro in enumerate(resumen_nuevoingreso, start=1):
+            self.tabla.insert("", "end", values=(str(registro[0]), registro[1], registro[2], str(registro[3]), registro[4]))
+
+        # Detectar selección de fila
+        self.tabla.bind("<<TreeviewSelect>>", self.on_select_row)
 
     def mostrar_buscar_producto(self):
-        """
-        Muestra la vista para buscar un producto.
-        """
         self.root.mainloop()
 
     def volver_a_opciones(self):
-        """
-        Oculta la vista para buscar un producto y muestra la vista de opciones.
-        """
         self.root.withdraw()
         self.controller.mostrar_opciones()
 
-# Puedes agregar más lógica y elementos según tus necesidades
+    def get_selected_id(self):
+        # Obtener la selección actual
+        selected_items = self.tabla.selection()
+        
+        # Verificar si hay alguna fila seleccionada
+        if selected_items:
+            # Si hay una fila seleccionada, obtener su ID
+            selected_row = selected_items[0]
+            selected_id = self.tabla.item(selected_row, "values")[0]
+            return selected_id
+        else:
+            # Si no hay filas seleccionadas, devolver None o algún otro valor que indique que no hay selección
+            return None
 
-if __name__ == "__main__":
-    buscar_producto_root = tk.Tk()
-    buscar_producto_view = BuscarProductoView(buscar_producto_root, None)  # Se eliminó el espacio en blanco no imprimible
-    buscar_producto_view.mostrar_buscar_producto()
+
+    def on_select_row(self, event):
+        selected_id = self.get_selected_id()
+
+        # Imprimir el ID seleccionado por consola
+        print(f"ID seleccionado: {selected_id}")
+
+        # Crear una nueva ventana para la vista "buscar_producto_especifico_view"
+        nueva_ventana = tk.Toplevel(self.root)
+        nueva_ventana.title(f"Información del Producto - ID: {selected_id}")
+
+        # Pasar el ID seleccionado a la nueva vista
+        BuscarProductoEspecificoView(nueva_ventana, self.controller, selected_id)
+
+    def buscar_en_tabla(self):
+        # Obtener el texto de búsqueda
+        texto_busqueda = self.search_entry.get()
+
+        # Verificar si la caja de texto está vacía
+        if not texto_busqueda.strip():
+            messagebox.showwarning("Búsqueda vacía", "Su búsqueda está vacía.")
+            return
+
+        # Buscar primero por guía
+        resultados_guia = self.db_queries.search_by_guia(texto_busqueda)
+        if resultados_guia:
+            print("Se encontró en la guía.")
+            self.actualizar_tabla(resultados_guia)
+            return
+
+        # Buscar por empresa si no se encontró en la guía
+        resultados_empresa = self.db_queries.search_by_empresa(texto_busqueda)
+        if resultados_empresa:
+            print("Se encontró en la empresa.")
+            self.actualizar_tabla(resultados_empresa)
+            return
+
+        # Buscar por nombre de producto si no se encontró en la guía ni en la empresa
+        resultados_producto = self.db_queries.search_by_nombre_producto(texto_busqueda)
+        if resultados_producto:
+            print("Se encontró en productos.")
+            self.actualizar_tabla(resultados_producto)
+            return
+
+        # Buscar por serie si no se encontró en la guía, empresa ni en el nombre del producto
+        resultados_serie = self.db_queries.search_by_serie(texto_busqueda)
+        if resultados_serie:
+            print("Se encontró en serie.")
+            self.actualizar_tabla(resultados_serie)
+            return
+
+        # Si no se encontraron resultados en ninguno de los casos anteriores
+        messagebox.showwarning("Búsqueda sin resultados", "No se encontraron resultados para la búsqueda realizada.")
+
+    def actualizar_tabla(self, resultados):
+        # Limpiar la tabla
+        for row in self.tabla.get_children():
+            self.tabla.delete(row)
+
+        # Insertar datos
+        for id_tuple in resultados:
+            id_entero = int(id_tuple[0])
+            # Obtener datos completos para el ID
+            datos_completos = self.db_queries.obtener_datos_completos(id_entero)
+            if datos_completos:
+                # Insertar datos en la tabla
+                self.tabla.insert("", "end", values=(id_entero, *datos_completos[0]))
+                print(datos_completos)
+
+    def borrar_tabla(self):
+        # Limpiar la tabla
+        for row in self.tabla.get_children():
+            self.tabla.delete(row)
+        
+        # Limpiar el contenido de la caja de texto
+        self.search_entry.delete(0, tk.END)
+        
+        # Mostrar resumen de 'NuevoIngreso'
+        self.mostrar_resumen_nuevoingreso()
+        
+        # Dar foco al cuadro de búsqueda
+        self.search_entry.focus_set()
